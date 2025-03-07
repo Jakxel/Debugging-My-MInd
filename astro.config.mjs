7990349c2f2e07ba4cf579a29d644a42e94cd1fd
Victor Jakxel Islas Carreon <jakxel20@gmail.com>
2025-03-07 15:58:11 -0800

    new changes for pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+export default {
+  site: 'https://Jakxel.github.io/debugging-my-mind', // URL de tu repositorio en GitHub Pages
+};
